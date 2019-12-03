2f0023065a61d9292986af739f34cc979a1c65b4
jonathan-crepeau <43802860+jonathan-crepeau@users.noreply.github.com>
2019-12-02 18:53:00 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User fipped queen");
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped queen");
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You've found a match!");
+} else {
+  alert ("Sorry, try again.");
+}
+}
